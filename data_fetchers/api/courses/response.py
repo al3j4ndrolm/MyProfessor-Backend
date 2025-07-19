0013c094d6e92621d025794def3fa469a53a525a
al3j4ndrolm <lopezalejandro5b@gmail.com>
2025-07-19 15:35:48 -0700

    kkk

--- data_fetchers/api/courses/response.py
+++ data_fetchers/api/courses/response.py
@@ -1,7 +1,14 @@
 
 import json
 
+<<<<<<< Updated upstream
 
+=======
+<<<<<<< HEAD
+=======
+
+>>>>>>> d59aa3e62d23c8e501e9418a7b7cc76a80c770b1
+>>>>>>> Stashed changes
 def create_courses_data(department_name: str, courses_full_name_list: list) -> dict:
     """
     Example of return value:
@@ -20,5 +27,9 @@ def create_courses_data(department_name: str, courses_full_name_list: list) -> d
 
 
 if __name__ == "__main__":  
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
     print(json.dumps(create_courses_data("ACCT - Accounting", ["ACCT 64 - Payroll and Business Tax Accounting", "ACCT 1C - Managerial Accounting"]), indent=2))
 
