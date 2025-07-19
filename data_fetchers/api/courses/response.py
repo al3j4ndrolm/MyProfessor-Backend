6c0b69232ebd82c4efb6b6e2943a3758b4b7201e
al3j4ndrolm <lopezalejandro5b@gmail.com>
2025-07-19 15:36:59 -0700

    kkk

--- data_fetchers/api/courses/response.py
+++ data_fetchers/api/courses/response.py
@@ -2,12 +2,18 @@
 import json
 
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 
 =======
+=======
+>>>>>>> Stashed changes
 <<<<<<< HEAD
 =======
 
 >>>>>>> d59aa3e62d23c8e501e9418a7b7cc76a80c770b1
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 def create_courses_data(department_name: str, courses_full_name_list: list) -> dict:
     """
@@ -28,6 +34,10 @@ def create_courses_data(department_name: str, courses_full_name_list: list) -> d
 
 if __name__ == "__main__":  
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 =======
 
 >>>>>>> Stashed changes
