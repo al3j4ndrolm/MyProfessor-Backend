51bbc0a3eb8171ea52f9b57e1042f424af271270
al3j4ndrolm <lopezalejandro5b@gmail.com>
2025-07-19 16:20:23 -0700

    remove merge issue lines

--- data_fetchers/api/courses/response.py
+++ data_fetchers/api/courses/response.py
@@ -1,20 +1,6 @@
 
 import json
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-
-=======
-=======
->>>>>>> Stashed changes
-<<<<<<< HEAD
-=======
-
->>>>>>> d59aa3e62d23c8e501e9418a7b7cc76a80c770b1
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 def create_courses_data(department_name: str, courses_full_name_list: list) -> dict:
     """
     Example of return value:
@@ -33,13 +19,5 @@ def create_courses_data(department_name: str, courses_full_name_list: list) -> d
 
 
 if __name__ == "__main__":  
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
-=======
-
->>>>>>> Stashed changes
     print(json.dumps(create_courses_data("ACCT - Accounting", ["ACCT 64 - Payroll and Business Tax Accounting", "ACCT 1C - Managerial Accounting"]), indent=2))
 
