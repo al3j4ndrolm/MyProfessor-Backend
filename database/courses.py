207ea74f56ba21d7d889c9712f754af1da4ce03c
al3j4ndrolm <lopezalejandro5b@gmail.com>
2025-07-23 18:35:16 -0700

    Update courses.py

--- database/courses.py
+++ database/courses.py
@@ -10,6 +10,9 @@ key = os.getenv("SUPABASE_KEY")
 
 <<<<<<< HEAD
 courses_table: Client = create_client(url, key).table("courses")
+=======
+supabase: Client = create_client(url, key)
+>>>>>>> 2bc1b5850e62bd7a55151af3b18dce93e85c7290
 
 def save_courses_data(courses_data_table: dict, school_name: str):
 
@@ -20,6 +23,9 @@ def save_courses_data(courses_data_table: dict, school_name: str):
     }
 <<<<<<< HEAD
     courses_table.insert(data).execute()
+=======
+    supabase.table("courses").insert(data).execute()
+>>>>>>> 2bc1b5850e62bd7a55151af3b18dce93e85c7290
 
 # if __name__ == "__main__":    
 #     school_name = "San Jose State University"
