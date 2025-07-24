f25ca3cd32c82c80e0e7a6920b3b9b3119b94936
al3j4ndrolm <lopezalejandro5b@gmail.com>
2025-07-24 15:17:55 -0700

    Comment fixes #1

--- data_fetchers/api/schools/configs.py
+++ data_fetchers/api/schools/configs.py
@@ -1,5 +1,14 @@
+# Broadcasts
+BROADCAST_ID_KEY = "broadcastId"
+TEXT_KEY = "text"
+NEED_UPDATE_KEY = "isUpdate"
+MIN_VERSION_KEY = "minVersion"
+PLATFORM_KEY = "platform"
+
+# Schools
 SCHOOL_NAME_KEY = "schoolName"
 RMP_CODE_KEY = "rmpCode"
 TERMS_KEY = "terms"
 NOTIFICATION_KEY = "notification"
 STATUS_KEY = "status"
+
--- data_fetchers/api/schools/response.py
+++ data_fetchers/api/schools/response.py
@@ -1,16 +1,21 @@
 from supabase import Client
 
 from database.schools import get_schools_data
-from data_fetchers.api.schools.configs import SCHOOL_NAME_KEY, RMP_CODE_KEY, TERMS_KEY, NOTIFICATION_KEY, STATUS_KEY
+from database.broadcasts import get_broadcasts_data
+from data_fetchers.api.schools.configs import SCHOOL_NAME_KEY, RMP_CODE_KEY, TERMS_KEY, NOTIFICATION_KEY, STATUS_KEY, TEXT_KEY, NEED_UPDATE_KEY, MIN_VERSION_KEY, PLATFORM_KEY, BROADCAST_ID_KEY
 
-def create_schools_data(supabase: Client) -> dict:
+def create_schools_response(supabase: Client) -> dict:
     schools_data = get_schools_data(supabase)
-
+    broadcasts_data = get_broadcasts_data(supabase)
     school_list = []
+    broadcast_list = []
+
     for entry in schools_data:
         school_list.append(create_school_list(entry))
+    for entry in broadcasts_data:
+        broadcast_list.append(create_broadcast_list(entry))
 
-    return {"schools": school_list, "broadcasts": []}
+    return {"schools": school_list, "broadcasts": broadcast_list}
 
 def create_school_list(entry: dict) -> dict:
     return {
@@ -19,4 +24,14 @@ def create_school_list(entry: dict) -> dict:
         TERMS_KEY: entry["terms"],
         NOTIFICATION_KEY: {"text": entry["notification"]},
         STATUS_KEY: entry["status"]
+    }
+
+def create_broadcast_list(entry: dict) -> dict:
+
+    return {
+        BROADCAST_ID_KEY: entry["id"],
+        TEXT_KEY: entry["text"],
+        NEED_UPDATE_KEY: entry["need_update"],
+        MIN_VERSION_KEY: entry["min_version"],
+        PLATFORM_KEY: entry["platform"]
     }
