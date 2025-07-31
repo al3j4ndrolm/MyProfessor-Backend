6ada18dfc034626f234472d79bd0c60238fb6ad0
al3j4ndrolm <lopezalejandro5b@gmail.com>
2025-07-30 18:39:58 -0700

    Create departments.py

---
+++ data_fetchers/schools/sfsu/departments.py
@@ -0,0 +1,33 @@
+import logging
+from bs4 import BeautifulSoup
+import re
+
+logger = logging.getLogger(__name__)
+
+def get_departments(soup: BeautifulSoup) -> list[dict]:
+    """
+    Get the departments from the soup.
+    """
+    try:
+        logger.debug("Extracting departments from SFSU")
+
+        department_elements_holder = soup.find("div", id="content")
+        script_elements = department_elements_holder.find_all("script")
+        department_elements = script_elements[2:len(script_elements)-2]
+
+        department_name_code_list = []
+        for department_element in department_elements:
+            subject_code = re.search(r'var subjectCode = "(.*?)";', department_element.text).group(1)
+            subject_description = re.search(r'var subjectDescription = "(.*?)";', department_element.text).group(1)
+            department_name_code_list.append((f"{subject_code} - {subject_description}", subject_code))
+
+        return department_name_code_list
+
+    except Exception as e:
+        logger.error(f"Error extracting departments: {e}")
+    
+if __name__ == "__main__":
+    with open("tests/test_samples/sfsu/terms_test_sample.html", "r") as file:
+        soup = BeautifulSoup(file, "html.parser")
+        departments = get_departments(soup)
+        print(departments)
