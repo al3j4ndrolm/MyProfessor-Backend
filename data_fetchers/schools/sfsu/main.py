2bfa4f4d5365faf093fc843e4484d7b7b1983782
al3j4ndrolm <lopezalejandro5b@gmail.com>
2025-07-31 13:09:07 -0700

    Create main.py

---
+++ data_fetchers/schools/sfsu/main.py
@@ -0,0 +1,49 @@
+from logger import logger
+import time, requests
+
+def get_session_data(department_code: str, term_code: str):
+
+    """
+    Example of return value:
+    {
+        "aaData": [
+            [
+                "<a>PHYS 200 [01]</a>",
+                "REG",
+                "Physics 200",
+                "3",
+                "1",
+                "1",
+                "1",
+    }
+    """
+    
+    try:
+        session = requests.Session()
+        # 1) prime your session with the filters
+        session.get(
+            "https://webapps.sfsu.edu/public/classservices/classsearch/results",
+            params={
+                "searchFor":     department_code,   # ← your department code
+                "term":          term_code,
+                "classCategory": "REG",
+            }
+        )
+    except Exception as e:
+        logger.error(f"Error getting session for {department_code} set per term {term_code}: {e}")
+
+    # 2) now fetch the JSON (timestamp only matters to bust cache)
+    try:
+        r = session.get(
+            "https://webapps.sfsu.edu/public/classservices/searchresultsjson",
+            params={"_": int(time.time() * 1000)}
+        )
+    except Exception as e:
+        logger.error(f"Error getting JSON for {department_code} set per term {term_code}: {e}")
+
+    try:
+        data = r.json()
+        return data
+    except Exception as e:
+        logger.error(f"Error getting data for {department_code} set per term {term_code}: {e}")
+        return {}
