2b30682dd02ef8242f2ca8aa24c9cdee81e11a2e
al3j4ndrolm <lopezalejandro5b@gmail.com>
2025-07-31 13:09:34 -0700

    Update courses.py

--- data_fetchers/schools/sfsu/courses.py
+++ data_fetchers/schools/sfsu/courses.py
@@ -6,7 +6,7 @@ import time, requests
 from bs4 import BeautifulSoup
 
 # we are going to pass a list of department codes and a term code to update the courses set
-def update_courses_set_per_term(department_code: str, term_code: str, courses_set: set) -> set:
+def update_courses_set_per_term(session_data: dict, courses_set: set) -> set:
     """
     Example of return value:
     {
