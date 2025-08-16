e15f9b7d1b7e01a5958de1049a83220d77e88b62
al3j4ndrolm <lopezalejandro5b@gmail.com>
2025-08-16 16:50:25 -0700

    fix broken script

--- data_fetchers/schools/ccsf/main.py
+++ data_fetchers/schools/ccsf/main.py
@@ -7,7 +7,7 @@ from helpers.soup_getter import html_url_to_soup
 from helpers.data import data_keys
 from data_fetchers.ratings.rating_provider import get_ratings_and_merge
 from data_fetchers.schools.ccsf.terms import get_terms
-from data_fetchers.schools.ccsf.courses import get_courses_per_department
+from data_fetchers.schools.ccsf.courses import update_courses_data_table    
 from data_fetchers.schools.ccsf.schedules import get_classes_per_department
 from data_fetchers.schools.ccsf.departments import get_department_data_table
 from data_fetchers.schools.ccsf.school_config import TERMS_BASE_URL, SCHEDULES_BASE_URL, SCHOOL_NAME, RMP_CODE
@@ -62,8 +62,7 @@ def get_courses_and_classes(department_data_table: dict, term_codes: list) -> tu
             classes_data_table[term_code][department_code] = classes_per_department
 
             logger.debug(f"Extracting courses for {department_code} in {term_code} ...")
-            courses_per_term = get_courses_per_department(department_code, department_soup)
-            courses.update(courses_per_term)
+            update_courses_data_table(department_soup, courses_data_table, department_code)
 
             time.sleep(1)
 
