cf56a5c24a462b30004e03b6943268b90cb16978
sheepyd3v <bizarr.development@gmail.com>
2026-07-14 15:38:54 -0700

    send suffix in separate field

--- api/configs.py
+++ api/configs.py
@@ -3,6 +3,7 @@ SCHOOLS_KEY_SCHOOL_LIST = "schools"
 SCHOOLS_KEY_BROADCASTS = "broadcasts"
 
 SCHOOL_NAME = "schoolName"
+SCHOOL_NAME_SUFFIX = "schoolNameSuffix"
 SCHOOL_TERMS = "terms"
 SCHOOL_NOTIFICATION = "notification"
 SCHOOL_STATUS = "status"
